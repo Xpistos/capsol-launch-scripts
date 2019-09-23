7bae57156e848abe7ec9b9e5221ae6a69c347271
wertyy102 <mykeyboardisqwerty@gmail.com>
2019-09-22 21:47:38 -0500

    Update debian-minecraft-oneshot.sh
    moar work on the systemstart.sh

--- debian-minecraft-oneshot.sh
+++ debian-minecraft-oneshot.sh
@@ -6,12 +6,12 @@ sysmem = "grep MemTotal /proc/meminfo | awk '{print $2}'"
 
 apt update
 apt upgrade -y
-apt install qemu-guest-agent vim neofetch htop screen wget curl openjdk-8-jdk python python3 python-pip python3-pip
+apt install -y qemu-guest-agent vim neofetch htop screen wget curl openjdk-8-jdk python python3 python-pip python3-pip
 
 mkdir -p /opt/minecraft/server
 wget "https://launcher.mojang.com/v1/objects/3dc3d84a581f14691199cf6831b71ed1296a9fdf/server.jar" -O /opt/minecraft/server/server.jar
 
-echo "java -Xmx($sysmem) -Xms($sysmem) -jar server.jar nogui" >> /opt/minecraft/server/serverstart.sh
+cat java -Xmx"sysmem"K -Xms"$sysmem"K -jar server.jar nogui >> /opt/minecraft/server/serverstart.sh
 chmod 777 /opt/minecraft/server/serverstart.sh
 
 
