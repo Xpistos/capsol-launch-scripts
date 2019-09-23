21eadc7b69653f72cd20d76ada6d5b957c7e7289
wertyy102 <mykeyboardisqwerty@gmail.com>
2019-09-22 20:42:19 -0500

    First Commit!!!!!!
    Well, let's see how these go...

---
+++ debian-minecraft-oneshot.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+if [ -z "$SCRIPT" ]
+then 
+    /var/log/install-oneshot.log /bin/bash -c "$0 $*"
+    exit 0
+fi
+
+apt update
+apt upgrade -y
+apt install qemu-guest-agent vim neofetch htop screen wget curl openjdk-8-jdk python python3 python-pip python3-pip
+
+mkdir -p /opt/minecraft/server
+wget "https://launcher.mojang.com/v1/objects/3dc3d84a581f14691199cf6831b71ed1296a9fdf/server.jar" -O /opt/minecraft/server/server.jar
+
